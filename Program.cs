652a8df0446fcf80ba6ea80cfbbadd631902fd34
Retae18 <35357413+Retae18@users.noreply.github.com>
2018-01-23 12:13:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopAdkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            while (x < 30)
+            {
+                Console.WriteLine(x);
+                x+=2;
+            }
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
